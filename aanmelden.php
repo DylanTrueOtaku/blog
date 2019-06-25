6e14472a1771dabe62c5a287d8a9b3f8c77fd7f0
DylanTrueOtaku <dylan8.riet@gmail.com>
2019-06-25 07:49:02 +0200

    added onto the php

---
+++ aanmelden.php
@@ -0,0 +1,13 @@
+<?php
+if(isset($_POST["submit"])){
+  $fotoNaam =  basename($_FILES["foto"]["name"]);
+  global $uploadsmap;
+  function upload(){
+    global $uploadsmap
+  $uploadmap = "uploads/"
+  }
+}
+
+
+
+?>
