32bab34197fdf5f22939dddb087f94c5666fb3eb
DylanTrueOtaku <dylan8.riet@gmail.com>
2019-06-25 07:59:59 +0200

    .

--- aanmelden.php
+++ aanmelden.php
@@ -7,7 +7,28 @@ if(isset($_POST["submit"])){
     $uploadsMap = "uploads/";
     $uploadsMap = $uploadsMap . basename($FILES["foto"]["name"]);
     $fotoType = pathinfo($uploadsMap, PATHINFO_EXTENSION);
+
+    // controleer of de foto al bestaat
+    if (file_exists($uploadsMap)){
+      echo "Deze foto bestaat al.";
+      return false;
+    }
+
+    // valideer fotoSize
+    if ($_FILES["foto"]["size"] > 500000){
+      echo "Deze foto is te groot.";
+      return false;
+    }
+
+    // valideer formaat
+    if($fotoType != "jgp" &&
+      $fotoType != "png" &&
+      $fotoType != "jpeg" &&
+      $fotoType != "gif") {
+      echo
+    }
   }
+  return true;
 }
 
 
