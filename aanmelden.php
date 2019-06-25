cd2476f1c3ef3eaa80966b246dc3a86db876da06
DylanTrueOtaku <dylan8.riet@gmail.com>
2019-06-25 10:52:37 +0200

    .

--- aanmelden.php
+++ aanmelden.php
@@ -25,10 +25,31 @@ if(isset($_POST["submit"])){
       $fotoType != "png" &&
       $fotoType != "jpeg" &&
       $fotoType != "gif") {
-      echo
+      echo "Foto moet JPG, JPEG, PNG of Gif zijn.";
+      return false;
+    }
+    return true;
+  }
+
+  // verplaats foto van temp-map naar uploadsMap
+  if(upload()){
+    if (move_uploaded_file($_FILES["foto"]["tmp_name"],
+        $uploadsMap)){
+      echo "Foto is geupload.";
+
+      //gebruiker opslaan
+      $bestand=fopen("gebruikers.txt","ab");
+      if (!$bestand)
+      {
+        echo "Kon geen bestand openen"
+      }
+
+      $naam = htmlspecialchars($_POST['naam']);
+      $email = htmlspecialchars($_POST['password']);
+      $wachtwoord = htmlspecialchars($_POST['password']);
+      $profielfoyo
     }
   }
-  return true;
 }
 
 
